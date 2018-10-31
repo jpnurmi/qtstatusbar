0de9c667629f8fcb10164be664edc0eed694bc60
ekke <ekke@ekkes-corner.org>
2018-10-31 13:28:39 +0100

    fix: restore preferred StatsBarStyle onOrientationChanged (#5)

--- src/statusbar_ios.mm
+++ src/statusbar_ios.mm
@@ -25,6 +25,9 @@
 #include <UIKit/UIKit.h>
 #include <QGuiApplication>
 
+#include <QScreen>
+#include <QTimer>
+
 @interface QIOSViewController : UIViewController
 @property (nonatomic, assign) BOOL prefersStatusBarHidden;
 @property (nonatomic, assign) UIStatusBarAnimation preferredStatusBarUpdateAnimation;
@@ -56,6 +59,23 @@ static void setPreferredStatusBarStyle(UIWindow *window, UIStatusBarStyle style)
     [viewController setNeedsStatusBarAppearanceUpdate];
 }
 
+void togglePreferredStatusBarStyle()
+{
+    UIStatusBarStyle style = statusBarStyle(StatusBar::Light);
+    if(StatusBarPrivate::theme == StatusBar::Light) {
+        style = statusBarStyle(StatusBar::Dark);
+    }
+    UIWindow *keyWindow = [[UIApplication sharedApplication] keyWindow];
+    if (keyWindow)
+        setPreferredStatusBarStyle(keyWindow, style);
+    QTimer::singleShot(200, []() {
+        UIStatusBarStyle style = statusBarStyle(StatusBarPrivate::theme);
+        UIWindow *keyWindow = [[UIApplication sharedApplication] keyWindow];
+        if (keyWindow)
+            setPreferredStatusBarStyle(keyWindow, style);
+    });
+}
+
 static void updatePreferredStatusBarStyle()
 {
     UIStatusBarStyle style = statusBarStyle(StatusBarPrivate::theme);
@@ -71,5 +91,11 @@ void StatusBarPrivate::setTheme_sys(StatusBar::Theme)
     QObject::connect(qApp, &QGuiApplication::applicationStateChanged, qApp, [](Qt::ApplicationState state) {
         if (state == Qt::ApplicationActive)
             updatePreferredStatusBarStyle();
+    }, Qt::UniqueConnection);
+
+    QScreen *screen = qApp->primaryScreen();
+    screen->setOrientationUpdateMask(Qt::PortraitOrientation | Qt::LandscapeOrientation | Qt::InvertedPortraitOrientation | Qt::InvertedLandscapeOrientation);
+    QObject::connect(screen, &QScreen::orientationChanged, qApp, [](Qt::ScreenOrientation) {
+        togglePreferredStatusBarStyle();
     }, Qt::UniqueConnection);
 }
